3cbcffddd306b3b1a7431e3d86714a51d11bea0f
Vlad <sov4ik.steam@gmail.com>
2020-03-09 11:45:38 -0500

    Initial commit

---
+++ src/app/components/create-book/create-book.component.ts
@@ -0,0 +1,42 @@
+import { Book } from '../../common/book';
+import { BookService } from '../../_services/book.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-Book',
+  templateUrl: './create-book.component.html',
+  styleUrls: ['./create-book.component.css']
+})
+export class CreateBookComponent implements OnInit {
+
+  book: Book = new Book();
+  submitted = false;
+
+  constructor(private bookService: BookService,
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  newBook(): void {
+    this.submitted = false;
+    this.book = new Book();
+  }
+
+  save() {
+    this.bookService.createBook(this.book)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.book = new Book();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
+
+  gotoList() {
+    this.router.navigate(['/books']);
+  }
+}
